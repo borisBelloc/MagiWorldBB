c50558febc2e01df1aeafeb5f20730df228e7c49
Boris BELLOC <bb.ItWork@gmail.com>
2019-07-31 11:02:47 +0200

    implemented askCharacterCreation()

--- src/com/bb/Game.java
+++ src/com/bb/Game.java
@@ -1,7 +1,30 @@
 package com.bb;
 
+import java.util.Scanner;
+
 public class Game {
+    String player1Char;
+    String player2Char;
+
+    public void start() {
+        System.out.println("Création du personnage du Joueur 1");
+    }
 
 
+    public int askCharacterCreation() {
+        int choice = -1;
+        Scanner scanChoice = new Scanner(System.in);
+//        choice = scanChoice.nextInt();
+
+        while (!( choice > 0 && choice <= 3)) {
+            System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
+            // TODO ? Après une erreur, on vide le scanner afin de pouvoir surveiller le prochain int saisie
+            //            scanChoice.next();
+            choice = scanChoice.nextInt();
+        }
+
+            System.out.println("---->" + choice);
+        return choice;
+    }
 
 }
--- src/com/bb/Main.java
+++ src/com/bb/Main.java
@@ -3,6 +3,10 @@ package com.bb;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Game game = new Game();
+        game.start();
+        game.askCharacterCreation();
+
     }
 }
