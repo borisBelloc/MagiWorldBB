b6d4d53a06c5153729f8f498b9dd2f4be1cfbf1c
Boris BELLOC <bb.ItWork@gmail.com>
2019-07-31 14:30:38 +0200

    @Game; implemented Input verification (int && in allowed range)+ askCharacterCreation() do while)

--- src/com/bb/Game.java
+++ src/com/bb/Game.java
@@ -6,33 +6,68 @@ public class Game {
     String player1Char;
     String player2Char;
 
+
+    // ----------------------
+    //    Imprim text
+    // ----------------------
     public void start() {
         System.out.println("Création du personnage du Joueur 1");
     }
 
-    // Used to collect user's input (int)
-//    public int input() {
-//        Scanner sc = new Scanner(System.in);
-//        return sc.nextInt();
-//    }
+
+    /**
+     * Used to collect user's input and check if it's an int
+     * @return user choice as int
+     */
+    // TODO ? Après une erreur, on vide le scanner afin de pouvoir surveiller le prochain int saisie
+    //            scanChoice.next();
+    public int userInputInt() {
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNextInt()) {
+            String input = sc.next();
+            System.out.printf("\"%s\" n'est pas valide ! Veuillez entrer un nombre entier.\n", input);
+        }
+        return sc.nextInt();
+    }
+
+
+    /**
+     * Check if the user input is in the allowed choice range
+     * @param userChoice
+     * @param origin
+        // origin (allowed value):
+        // 1 : askCharacterCreation : 0 < x <= 3
+        // 2 : attributs selection : x < level
+        // 3 : combat selection ? : 1 || 2
+     * @return boolean
+     */
+    public boolean isUserInputOk(int origin, int userChoice) {
+        switch (origin) {
+            case 1:
+                if (userChoice > 0 && userChoice <= 3) {
+                    return true;
+                } else {
+                    return false;
+                }
+//            case 2:
+//                break;
+//            case 3:
+//                break;
+        }
+//        TODO: voir ce return à fix
+        return false;
+    }
 
     public int askCharacterCreation() {
-        int choice = -1;
-        Scanner scanChoice = new Scanner(System.in);
+//        int choice = -1;
 
-        while (!( choice > 0 && choice <= 3)) {
+        do {
             System.out.println("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)");
-            // TODO ? Après une erreur, on vide le scanner afin de pouvoir surveiller le prochain int saisie
-            //            scanChoice.next();
-
-            // if user enter a String, tell him that's wrong
-            while (!scanChoice.hasNextInt()) {
-                String input = scanChoice.next();
-                System.out.printf("\"%s\" Veuillez entrer un nombre entre 1 et 3.\n", input);
-            }
-            choice = scanChoice.nextInt();
         }
-        return choice;
+        while (!isUserInputOk(1, userInputInt() ));
+
+//        return choice;
+        return 0;
     }
 
 }
