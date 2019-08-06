a0426b0c0101ca072ba3b8b7637a2dff83c8ea29
Boris BELLOC <bb.ItWork@gmail.com>
2019-08-06 16:47:26 +0200

    Implemented rogue class

---
+++ src/com/bb/Rogue.java
@@ -0,0 +1,38 @@
+package com.bb;
+
+public class Rogue extends Character {
+    public Rogue(String playerOwner, int level, int strength, int agility, int intelligence, int life) {
+        super(playerOwner, level, strength, agility, intelligence, life);
+    }
+
+    @Override
+    int basicAttack() {
+        // Damage equal to his agility
+        return getAgility();
+    }
+
+    @Override
+    int[] specialAttack() {
+        // Gain agility as (level/2)
+        int[] specialAttack = new int[1];
+        specialAttack[0] = getLevel() * 2;
+        return specialAttack;
+    }
+
+    @Override
+    String getDescription() {
+        return String.format("shhhhh je suis le Rôdeur %s niveau %d " +
+                        "je possède %d de vitalité, %d de force, %d d'agilité et %d d'intelligence !",
+                getPlayerOwner(), getLevel(), getLife(), getStrength(), getAgility(), getIntelligence());
+    }
+
+    @Override
+    String getBasicAttackName() {
+        return "Tir à l’Arc";
+    }
+
+    @Override
+    String getSpecialAttackName() {
+        return "Concentration ";
+    }
+}
