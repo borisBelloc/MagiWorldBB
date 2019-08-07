a93a2942ca5943cd4ab643b4ca4cd63d1362d661
Boris BELLOC <bb.ItWork@gmail.com>
2019-08-07 18:40:40 +0200

    signed

--- src/com/bb/Main.java
+++ src/com/bb/Main.java
@@ -1,9 +1,12 @@
 package com.bb;
 
+/**
+ * Version 1 : 2019/08/07
+ * BELLOC Boris
+ */
 public class Main {
 
     public static void main(String[] args) {
-
         Game game = new Game();
         game.start();
     }
