3a745ee103f5355dfd87c8255d237ca3f35eee4d
Boris BELLOC <bb.ItWork@gmail.com>
2019-08-07 11:46:49 +0200

    added Test for isUserInputOk() for all origin

--- test/com/bb/GameTest.java
+++ test/com/bb/GameTest.java
@@ -5,26 +5,62 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
-    @Test
-    void userInputInt() {
-//        Si l'user entre un String -> valeur attendu <copier/coller du text "n'est pas valide...">;
-
-//        Si l'user entre un entier -> valeur attendu <la valeur entrée par l'user>
-    }
-
 
     // * Test isUserInputOk
+    // origin 1
     @Test
-    public void Given_Origin1AndWrongUserChoice_When_isUserInputOk_Then_DisplayCorrectProcess() {
+    public void Given_Origin1AndWrongUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
         Game game = new Game();
         assertEquals(false, game.isUserInputOk( 1, 5));
-    }    @Test
-    public void Given_Origin1AndRightUserChoice_When_isUserInputOk_Then_DisplayCorrectProcess() {
+    }
+    @Test
+    public void Given_Origin1AndRightUserChoice_When_isUserInputOk_Then_ReturnCorrectBools() {
         Game game = new Game();
         assertEquals(true, game.isUserInputOk( 1, 2));
     }
+    // origin 20
+    @Test
+    public void Given_Origin20AndWrongUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
+        Game game = new Game();
+        assertEquals(false, game.isUserInputOk( 20, 0));
+    }
+    @Test
+    public void Given_Origin20AndRightUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
+        Game game = new Game();
+        assertEquals(true, game.isUserInputOk( 20, 75));
+    }
+    // origin 21
+    @Test
+    public void Given_Origin21AndWrongUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
+        Game game = new Game();
+        assertEquals(false, game.isUserInputOk( 21, 110));
+    }
+    @Test
+    public void Given_Origin21AndRightUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
+        Game game = new Game();
+        assertEquals(true, game.isUserInputOk( 21, 75));
+    }
+    // origin 3
+    @Test
+    public void Given_Origin3AndWrongUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
+        Game game = new Game();
+        assertEquals(false, game.isUserInputOk( 3, 3));
+    }
+    @Test
+    public void Given_Origin3AndRightUserChoice_When_isUserInputOk_Then_ReturnCorrectBool() {
+        Game game = new Game();
+        assertEquals(true, game.isUserInputOk( 3, 1));
+    }
 
 
+
+    // * Test Création class
+//    @Test
+//    public void aaaa() {
+//        Game game = new Game();
+//        int userChoice = 1;
+//        assertEquals(false, game.askCharacterClass());
+//    }
 
 
 
