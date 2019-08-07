c973a025f4b5b3cb55d708272c760da6e2972d8b
Boris BELLOC <bb.ItWork@gmail.com>
2019-08-07 14:59:19 +0200

    added test for basic and special attack

--- test/com/bb/WarriorTest.java
+++ test/com/bb/WarriorTest.java
@@ -2,17 +2,42 @@ package com.bb;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarriorTest {
+        Game game = new Game();
 
     @Test
     public void Given_CharactersAttributs_When_WarriorCreated_Then_DisplayCorrectDescription() {
-        Game game = new Game();
         Character character = new Warrior(
         "Test", 10, 5, 4, 1, 50);
         String expectedDescription = "Woarg je suis le Guerrier Test niveau 10 " +
                 "je possède 50 de vitalité, 5 de force, 4 d'agilité et 1 d'intelligence !";
         assertEquals(expectedDescription, character.getDescription());
     }
+
+    @Test
+    public void Given_2Character_When_currentPlayerBasicAttackEnemy_Then_getTheRightHeal() {
+        int[] characterAttributes = {10, 10, 0, 0, 50};
+        game.characterCreation("Guerrier", characterAttributes, "player1");
+        game.characterCreation("Guerrier", characterAttributes, "player2");
+        Character current = game.charactersList.get(0);
+        Character enemy = game.charactersList.get(1);
+        enemy.setLife( enemy.getLife() - current.basicAttack());
+        assertEquals(40 , enemy.getLife());
+    }
+
+    @Test
+    public void Given_2Character_When_currentPlayerSpecialttackEnemy_Then_getTheRightHeal() {
+        int[] characterAttributes = {4, 10, 0, 0, 20};
+        game.characterCreation("Guerrier", characterAttributes, "player1");
+        game.characterCreation("Guerrier", characterAttributes, "player2");
+        System.setIn(new ByteArrayInputStream("2".getBytes()));
+        game.fight(game.charactersList);
+        assertEquals(15 , game.charactersList.get(0).getLife());
+        assertEquals(0 , game.charactersList.get(1).getLife());
+    }
+
 }
